c343330aa9685849593ff496bf03ba526f040899
JayyyLiu <37936987+JayyyLiu@users.noreply.github.com>
2022-02-18 01:25:28 -0600

    Add files via upload

--- naivebayesPXY.py
+++ naivebayesPXY.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 @author: Nigel
+@author: Yichen
+@author: M.Joo
 """
 
 import numpy as np
@@ -14,40 +16,47 @@ def naivebayesPXY(x, y):
 #    Input:
 #    x : n input vectors of d dimensions (dxn)
 #    y : n labels (-1 or +1) (1xn)
-#    
+#
 #    Output:
 #    posprob: probability vector of p(x|y=1) (dx1)
 #    negprob: probability vector of p(x|y=-1) (dx1)
 # =============================================================================
 
 
-    
+
     # Convertng input matrix x and y into NumPy matrix
     # input x and y should be in the form: 'a b c d...; e f g h...; i j k l...'
+    # TODO: do not use np.matrix!
     X = np.matrix(x)
     Y = np.matrix(y)
-    
-    # Pre-configuring the size of matrix X
+
     d,n = X.shape
-    
+
     # Pre-constructing a matrix of all-ones (dx2)
     X0 = np.ones((d,2))
-    Y0 = np.matrix('-1, 1')
-    
+    Y0 = np.array([[-1, 1]])
+
     # add one all-ones positive and negative example
     Xnew = np.hstack((X, X0)) #stack arrays in sequence horizontally (column-wise)
-        #Xnew = np.concatenate((X, X0), axis=1) #concatenate to column
     Ynew = np.hstack((Y, Y0))
-    
+
+    # matrix of all-zeros -
+    X1 = np.zeros((d, 2))
+    # add one all-zeros positive and negative example - M.Joo
+    Xnew = np.hstack((Xnew, X1))
+    Ynew = np.hstack((Ynew, Y0))
+
     # Re-configuring the size of matrix Xnew
     d,n = Xnew.shape
-    
+
 # =============================================================================
 # fill in code here
+    # YOUR CODE HERE
     sum_Ynew_pos = np.sum(np.where(Ynew<0,0,1))
     sum_Ynew_neg = n-sum_Ynew_pos
     posprob = np.dot(Xnew, np.where(Ynew<0,0,1).T)/sum_Ynew_pos
     negprob = np.dot(Xnew, np.where(Ynew>0,0,1).T)/sum_Ynew_neg
+
     return posprob,negprob
 
 # =============================================================================
